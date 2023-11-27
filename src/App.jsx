a71ae681591659789507ca01c73c3a6c0ab151b2
ravichandrapalla <cravi6635@gmail.com>
2023-11-27 21:11:33 +0530

    layout set with basic fonts and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+
 import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
 
 function App() {
   return (
     <>
-      <GlobalStyles></GlobalStyles>
-      <div>Hello World</div>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import styled from "styled-components";
 
 const StyledLogo = styled.div`
@@ -12,7 +13,7 @@ const Img = styled.img`
 function Logo() {
   return (
     <StyledLogo>
-      <Img src="/logo-light.png" alt="Logo" />
+      <Img src="./src/data/img/logo-light.png" alt="Logo" />
     </StyledLogo>
   );
 }
