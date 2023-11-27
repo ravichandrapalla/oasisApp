096ace787852b7edd7424dd4e46b356de63c48c5
ravichandrapalla <cravi6635@gmail.com>
2023-11-27 18:45:42 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles></GlobalStyles>
+      <div>Hello World</div>
+    </>
+  );
+}
+
+export default App;
