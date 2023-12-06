6de64f85747aa901c1188ebd6610d0235d0c3e7e
ravichandrapalla <cravi6635@gmail.com>
2023-12-06 18:52:17 +0530

    donw with booking related features

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -16,6 +16,10 @@ import { useNavigate } from "react-router-dom";
 
 import { HiArrowUpOnSquare } from "react-icons/hi2";
 import useCheckout from "../check-in-out/useCheckout";
+import Modal from "../../ui/Modal";
+import Booking from "./../../pages/Booking";
+import ConfirmDelete from "../../ui/ConfirmDelete";
+import { useDeleteBooking } from "./useDeleteBooking";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -26,6 +30,7 @@ const HeadingGroup = styled.div`
 function BookingDetail() {
   const { booking, isLoading } = useBooking();
   const { checkout, isCheckingOut } = useCheckout();
+  const { deleteBooking, isDeleting } = useDeleteBooking();
   const navigate = useNavigate();
   const moveBack = useMoveBack();
 
@@ -66,6 +71,20 @@ function BookingDetail() {
             Check out
           </Button>
         )}
+        <Modal>
+          <Modal.Open opens="delete">
+            <Button variation="danger">Delete Booking</Button>
+          </Modal.Open>
+          <Modal.Window name="delete">
+            <ConfirmDelete
+              resourceName="booking"
+              onConfirm={() =>
+                deleteBooking(bookingId, { onSettled: () => navigate(-1) })
+              }
+              disabled={isDeleting}
+            />
+          </Modal.Window>
+        </Modal>
         <Button variation="secondary" onClick={moveBack}>
           Back
         </Button>
---
+++ src/features/bookings/useDeleteBooking.js
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from "react-query";
+import { toast } from "react-hot-toast";
+import { deleteBooking as deleteBookingApi } from "../../services/apiBookings";
+
+export function useDeleteBooking() {
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate: deleteBooking } = useMutation({
+    mutationFn: deleteBookingApi,
+    onSuccess: () => {
+      toast.success("succesfully deleted");
+      queryClient.invalidateQueries({
+        queryKey: ["bookings"],
+      });
+    },
+
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  return { isDeleting, deleteBooking };
+}
