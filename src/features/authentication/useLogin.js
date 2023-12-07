01bdf7487bdebdc63c7d555701b461cf45ad88fd
ravichandrapalla <cravi6635@gmail.com>
2023-12-07 22:47:26 +0530

    added new multi functional header and fixed authentication

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,14 +1,16 @@
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export default function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      navigate("/dashboard");
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/dashboard", { replace: true });
       console.log(user);
     },
     onError: (err) => {
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,20 @@
 import supabase from "./supabase";
 
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+  if (error) throw new Error(error.message);
+  return data;
+}
+
 export async function login({ email, password }) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
@@ -18,4 +33,10 @@ export async function getCurrentUser() {
   console.log(data);
   if (error) throw new Error(error.message);
   return data?.user;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) throw new Error("error in logging out");
 }
