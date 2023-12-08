a9a179b9ab26f909d061a2479fbeccaa8ef3039f
ravichandrapalla <cravi6635@gmail.com>
2023-12-08 12:03:17 +0530

    feature update: added account update funtionality and avatar upload

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,5 @@
-import supabase from "./supabase";
+/* eslint-disable no-unused-vars */
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -39,4 +40,34 @@ export async function logout() {
   const { error } = await supabase.auth.signOut();
 
   if (error) throw new Error("error in logging out");
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  //1. updating name or password seperately
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+  const { data, error } = await supabase.auth.updateUser(updateData);
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  //2. update avatar
+
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  //3. update user again with user
+  const { data: updatedUser, error: error2 } = supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+  if (error2) throw new Error(error2.message);
+
+  return updatedUser;
 }
