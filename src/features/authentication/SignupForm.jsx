fe3ed0349fa34a608423ec1e599be96bc1bb2b09
ravichandrapalla <cravi6635@gmail.com>
2023-12-09 19:35:05 +0530

    test

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,8 +3,9 @@ import { useForm } from "react-hook-form";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
+
 import { useSignup } from "./useSignup";
+import Input from "./../../ui/Input";
 
 // Email regex: /\S+@\S+\.\S+/
 
